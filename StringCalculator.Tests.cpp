d227dc5604ec81b8f2bb7bd8d302daaf5b124daf
YUK5COB <K.Yuvaraj@in.bosch.com>
2024-07-13 18:53:50 +0530

    Enable fourth test case

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -29,14 +29,14 @@ TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
     int result = add(input);
     ASSERT_EQ(result, expectedresult);
 }
-/*
+
 TEST(StringCalculatorAddTests, ExpectSumWithNewlineDelimiter) {
     int expectedresult = 6;
     const char*  input = "1\n2,3";
     int result =add(input);
     ASSERT_EQ(result, expectedresult);
 }
-
+/*
 TEST(StringCalculatorAddTests, IgnoreNumbersGreaterThan1000) {
     int expectedresult = 1;
     const char*  input = "1,1001";
