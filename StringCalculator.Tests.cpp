6802c11950eca25c116a19103687491264f4cb6a
YUK5COB <K.Yuvaraj@in.bosch.com>
2024-07-14 09:57:09 +0530

    Null test case

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -16,6 +16,13 @@ TEST(StringCalculatorAddTests, ExpectZeroForEmpty) {
 }
 
 
+TEST(StringCalculatorAddTests, ExpectZeroForNull) {
+    int expectedresult = 0;
+    const char* input = "\0";
+    int result = add(input);
+    ASSERT_EQ(result, expectedresult);
+}
+
 TEST(StringCalculatorAddTests, ExpectZeroForSingleZero) {
     int expectedresult = 0;
     const char* input = "0";
