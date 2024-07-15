caa6bb62c9dba99e2ddf131ecf68ea7abbef8b1b
YUK5COB <K.Yuvaraj@in.bosch.com>
2024-07-15 08:49:22 +0530

    new test case

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -58,6 +58,13 @@ TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
     ASSERT_EQ(result, expectedresult);
 }
 
+TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter2) {
+    int expectedresult = 3;
+    const char*  input = "/1,2";
+    int result = add(input);
+    ASSERT_EQ(result, expectedresult);
+}
+
 TEST(StringCalculatorAddTests, ExpectSumWithMultipleCustomDelimiter) {
     int expectedresult = 6;
     const char*  input = "//[***]\n1***2***3";
