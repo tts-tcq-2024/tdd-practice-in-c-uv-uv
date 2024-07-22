ac45dfd4a5d1201cf1b74703d31cf8d84cd4fc55
YUK5COB <K.Yuvaraj@in.bosch.com>
2024-07-22 08:39:19 +0530

    coverage check3

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include "StringCalculator.h"
-/*
+
 TEST(StringCalculatorAddTests, ExpectZeroFornointeger) {
     int expectedresult = 0;
     const char* input = "Hello, world!";
@@ -29,14 +29,14 @@ TEST(StringCalculatorAddTests, ExpectZeroForSingleZero) {
     int result = add(input);
     ASSERT_EQ(result, expectedresult);
 }
-*/
+
 TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
     int expectedresult = 3;
     const char*  input = "1,2";
     int result = add(input);
     ASSERT_EQ(result, expectedresult);
 }
-/*
+
 TEST(StringCalculatorAddTests, ExpectSumWithNewlineDelimiter) {
     int expectedresult = 6;
     const char*  input = "1\n2,3";
@@ -50,17 +50,17 @@ TEST(StringCalculatorAddTests, IgnoreNumbersGreaterThan1000) {
     int result =add(input);
     ASSERT_EQ(result, expectedresult);
 }
-*/
+
 TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
     int expectedresult = 3;
     const char*  input = "//;\n1;2";
     int result = add(input);
     ASSERT_EQ(result, expectedresult);
 }
-/*
+
 TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter2) {
     int expectedresult = 3;
-    const char*  input = "\n1,2";
+    const char*  input = "\n\n1,2";
     int result = add(input);
     ASSERT_EQ(result, expectedresult);
 }
@@ -78,4 +78,3 @@ TEST(StringCalculatorAddTests, ExpectExceptionwithNegative) {
     ASSERT_EQ(result, -1);
     ASSERT_STREQ(error_message, "Negative not allowed");
 }
-*/
